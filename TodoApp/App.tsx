200556cb54921800a5ea61c74c68c3a242112475
Jinho Jang <verycosy@kakao.com>
2021-11-23 00:49:26 +0900

    date head

--- TodoApp/App.tsx
+++ TodoApp/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import DateHead from './components/DateHead';
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  const today = new Date();
+
+  return (
+    <SafeAreaView>
+      <StatusBar backgroundColor="#26a69a" />
+      <DateHead date={today} />
+    </SafeAreaView>
+  );
 };
 
 export default App;
