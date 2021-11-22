0a78ab694c68d0d03dd7e6829e85d2d2fe2e2400
Jinho Jang <verycosy@kakao.com>
2021-11-23 01:09:59 +0900

    add todo, empty

--- TodoApp/App.tsx
+++ TodoApp/App.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
+import Empty from './components/Empty';
 
 const App = () => {
   const today = new Date();
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
 
 export default App;
