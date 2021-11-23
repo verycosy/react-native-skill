650be83f6d23178f08a46fd4f927e3e75a2a202c
verycosy <verycosy@kakao.com>
2021-11-23 20:38:41 +0900

    keyboard avoiding

--- TodoApp/App.tsx
+++ TodoApp/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import DateHead from './components/DateHead';
@@ -11,9 +11,15 @@ const App = () => {
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
-        <DateHead date={today} />
-        <Empty />
-        <AddTodo />
+        <KeyboardAvoidingView
+          behavior={Platform.select({
+            ios: 'padding',
+          })}
+          style={styles.avoid}>
+          <DateHead date={today} />
+          <Empty />
+          <AddTodo />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -23,6 +29,9 @@ const styles = StyleSheet.create({
   block: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  avoid: {
+    flex: 1,
   },
 });
 
--- TodoApp/components/AddTodo.tsx
+++ TodoApp/components/AddTodo.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
 
 function AddTodo() {
-  return <View style={styles.block} />;
+  const [text, setText] = useState('');
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="할일을 입력하세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
     height: 64,
-    backgroundColor: 'red',
+    paddingHorizontal: 16,
+    borderColor: '#bdbdbd',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 8,
   },
 });
 
