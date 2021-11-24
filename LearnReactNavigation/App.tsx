bba32bbf3e8209d029e7ccbc77f88bbfc8c1fc81
verycosy <verycosy@kakao.com>
2021-11-24 13:13:44 +0900

    material bottom tab

--- LearnReactNavigation/App.tsx
+++ LearnReactNavigation/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  getFocusedRouteNameFromRoute,
+  NavigationContainer,
+} from '@react-navigation/native';
 import {Text} from 'react-native';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 type TabParamList = {
   Home: undefined;
@@ -11,7 +16,8 @@ type TabParamList = {
   Message: undefined;
 };
 
-const Tab = createMaterialTopTabNavigator<TabParamList>();
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator<TabParamList>();
 
 function HomeScreen() {
   return <Text>Home</Text>;
@@ -29,59 +35,83 @@ function MessageScreen() {
   return <Text>Message</Text>;
 }
 
+function MainScreen() {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarColor: 'black',
+          tabBarLabel: '홈',
+          tabBarIcon: ({color}) => <Icon name="home" color={color} size={24} />,
+          tabBarBadge: 'new',
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarColor: 'gray',
+          tabBarLabel: '검색',
+          tabBarIcon: ({color}) => (
+            <Icon name="search" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarColor: 'green',
+          tabBarLabel: '알림',
+          tabBarIcon: ({color}) => (
+            <Icon name="notifications" color={color} size={24} />
+          ),
+          tabBarBadge: 30,
+        }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{
+          tabBarColor: 'blue',
+          tabBarLabel: '메시지',
+          tabBarIcon: ({color}) => (
+            <Icon name="message" color={color} size={24} />
+          ),
+          tabBarBadge: true,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home';
+  const nameMap = {
+    Home: '홈',
+    Search: '검색',
+    Notification: '알림',
+    Message: '메시지',
+  };
+
+  return nameMap[routeName];
+}
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          tabBarIndicatorStyle: {
-            backgroundColor: '#009688',
-          },
-          tabBarActiveTintColor: '#009688',
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: '홈',
-            tabBarIcon: ({color}) => (
-              <Icon name="home" color={color} size={24} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SearchScreen}
-          options={{
-            tabBarLabel: '검색',
-            tabBarIcon: ({color}) => (
-              <Icon name="search" color={color} size={24} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Notification"
-          component={NotificationScreen}
-          options={{
-            tabBarLabel: '알림',
-            tabBarIcon: ({color}) => (
-              <Icon name="notifications" color={color} size={24} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Message"
-          component={MessageScreen}
-          options={{
-            tabBarLabel: '메시지',
-            tabBarIcon: ({color}) => (
-              <Icon name="message" color={color} size={24} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{flex: 1}}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={({route}) => ({title: getHeaderTitle(route)})}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 };
 
