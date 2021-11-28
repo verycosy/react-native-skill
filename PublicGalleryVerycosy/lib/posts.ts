d2186cdde3c212216c26932f71fc1323b93d6984
Jinho Jang <verycosy@kakao.com>
2021-11-28 22:47:52 +0900

    refactor posts

--- PublicGalleryVerycosy/lib/posts.ts
+++ PublicGalleryVerycosy/lib/posts.ts
@@ -23,12 +23,27 @@ export function createPost({user, photoURL, description}: CreatePost) {
   });
 }
 
+interface GetPosts {
+  mode?: 'newer' | 'older';
+  id?: string;
+  userId?: string;
+}
+
 export const PAGE_SIZE = 3;
-export async function getPosts(userId?: string) {
+export async function getPosts({userId, mode, id}: GetPosts = {}) {
   let query = postsCollection.orderBy('createdAt', 'desc').limit(PAGE_SIZE);
 
   if (userId) {
     query = query.where('user.id', '==', userId);
+  }
+
+  if (id) {
+    const cursorDoc = await postsCollection.doc(id).get();
+
+    query =
+      mode === 'older'
+        ? query.startAfter(cursorDoc)
+        : query.endBefore(cursorDoc);
   }
 
   const snapshot = await query.get();
@@ -42,45 +57,9 @@ export async function getPosts(userId?: string) {
 }
 
 export async function getOlderPosts(id: string, userId?: string) {
-  const cursorDoc = await postsCollection.doc(id).get();
-
-  let query = postsCollection
-    .orderBy('createdAt', 'desc')
-    .startAfter(cursorDoc)
-    .limit(PAGE_SIZE);
-
-  if (userId) {
-    query = query.where('user.id', '==', userId);
-  }
-
-  const snapshot = await query.get();
-
-  const posts = snapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Post[];
-
-  return posts;
+  return getPosts({id, mode: 'older', userId});
 }
 
 export async function getNewerPosts(id: string, userId?: string) {
-  const cursorDoc = await postsCollection.doc(id).get();
-
-  let query = postsCollection
-    .orderBy('createdAt', 'desc')
-    .endBefore(cursorDoc)
-    .limit(PAGE_SIZE);
-
-  if (userId) {
-    query = query.where('user.id', '==', userId);
-  }
-
-  const snapshot = await query.get();
-
-  const posts = snapshot.docs.map(doc => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Post[];
-
-  return posts;
+  return getPosts({id, mode: 'newer', userId});
 }
