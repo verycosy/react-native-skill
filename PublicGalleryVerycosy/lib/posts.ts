3f1d4ac95513ddcdf8961821dbe9e2fc319c38ab
Jinho Jang <verycosy@kakao.com>
2021-11-28 22:38:17 +0900

    load older, newer posts

--- PublicGalleryVerycosy/lib/posts.ts
+++ PublicGalleryVerycosy/lib/posts.ts
@@ -23,8 +23,46 @@ export function createPost({user, photoURL, description}: CreatePost) {
   });
 }
 
+export const PAGE_SIZE = 3;
 export async function getPosts() {
-  const snapshot = await postsCollection.get();
+  const snapshot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .limit(PAGE_SIZE)
+    .get();
+
+  const posts = snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Post[];
+
+  return posts;
+}
+
+export async function getOlderPosts(id: string) {
+  const cursorDoc = await postsCollection.doc(id).get();
+
+  const snapshot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .startAfter(cursorDoc)
+    .limit(PAGE_SIZE)
+    .get();
+
+  const posts = snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Post[];
+
+  return posts;
+}
+
+export async function getNewerPosts(id: string) {
+  const cursorDoc = await postsCollection.doc(id).get();
+  const snapshot = await postsCollection
+    .orderBy('createdAt', 'desc')
+    .endBefore(cursorDoc)
+    .limit(PAGE_SIZE)
+    .get();
+
   const posts = snapshot.docs.map(doc => ({
     id: doc.id,
     ...doc.data(),
--- PublicGalleryVerycosy/screens/FeedScreen.tsx
+++ PublicGalleryVerycosy/screens/FeedScreen.tsx
@@ -1,24 +1,90 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList} from 'react-native';
+import {
+  ActivityIndicator,
+  FlatList,
+  RefreshControl,
+  StyleSheet,
+} from 'react-native';
 import PostCard from '../components/PostCard';
-import {getPosts, Post} from '../lib/posts';
+import {
+  getNewerPosts,
+  getOlderPosts,
+  getPosts,
+  PAGE_SIZE,
+  Post,
+} from '../lib/posts';
 
 function FeedScreen() {
   const [posts, setPosts] = useState<Post[]>();
+  const [noMorePost, setNoMorePost] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
     getPosts().then(setPosts);
   }, []);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id);
+
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+
+    setPosts(posts.concat(olderPosts));
+  };
+
+  const onRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return;
+    }
+
+    const firstPost = posts[0];
+    setRefreshing(true);
+
+    const newerPosts = await getNewerPosts(firstPost.id);
+    setRefreshing(false);
+
+    if (newerPosts.length === 0) {
+      return;
+    }
+
+    setPosts(newerPosts.concat(posts));
+  };
 
   return (
     <FlatList
       data={posts}
       renderItem={renderItem}
       keyExtractor={item => item.id}
+      contentContainerStyle={styles.container}
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.75}
+      ListFooterComponent={
+        !noMorePost ? (
+          <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+        ) : null
+      }
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
     />
   );
 }
 
 const renderItem = ({item}: {item: Post}) => <PostCard {...item} />;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 48,
+  },
+  spinner: {
+    height: 64,
+  },
+});
 
 export default FeedScreen;
