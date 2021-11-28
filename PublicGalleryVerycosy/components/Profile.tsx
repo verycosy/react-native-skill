6b1706632139c3e2b80d6691ccdb8042eddf6fa5
Jinho Jang <verycosy@kakao.com>
2021-11-28 23:43:17 +0900

    profile screen pagination

--- PublicGalleryVerycosy/components/Profile.tsx
+++ PublicGalleryVerycosy/components/Profile.tsx
@@ -2,11 +2,18 @@ import React, {useEffect, useState} from 'react';
 import {
   ActivityIndicator,
   FlatList,
+  RefreshControl,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import {getPosts, Post} from '../lib/posts';
+import {
+  getNewerPosts,
+  getOlderPosts,
+  getPosts,
+  PAGE_SIZE,
+  Post,
+} from '../lib/posts';
 import {getUser, User} from '../lib/users';
 import Avatar from './Avatar';
 import PostGridItem from './PostGridItem';
@@ -18,6 +25,42 @@ interface Props {
 function Profile({userId}: Props) {
   const [user, setUser] = useState<User>();
   const [posts, setPosts] = useState<Post[]>();
+
+  const [noMorePost, setNoMorePost] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost.id, userId);
+
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+
+    setPosts(posts.concat(olderPosts));
+  };
+
+  const onRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return;
+    }
+
+    const firstPost = posts[0];
+    setRefreshing(true);
+
+    const newerPosts = await getNewerPosts(firstPost.id, userId);
+    setRefreshing(false);
+
+    if (newerPosts.length === 0) {
+      return;
+    }
+
+    setPosts(newerPosts.concat(posts));
+  };
 
   useEffect(() => {
     getUser(userId).then(setUser);
@@ -46,6 +89,20 @@ function Profile({userId}: Props) {
           <Text style={styles.username}>{user.displayName}</Text>
         </View>
       }
+      onEndReached={onLoadMore}
+      onEndReachedThreshold={0.25}
+      ListFooterComponent={
+        !noMorePost ? (
+          <ActivityIndicator
+            style={styles.bottomSpinner}
+            size={32}
+            color="#6200ee"
+          />
+        ) : null
+      }
+      refreshControl={
+        <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+      }
     />
   );
 }
@@ -69,6 +126,9 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 24,
     color: '#424242',
+  },
+  bottomSpinner: {
+    height: 128,
   },
 });
 
