b8799d3ea19da5f3cbafd48cd4afcfe234653f7e
verycosy <verycosy@kakao.com>
2021-12-01 15:27:06 +0900

    redux todos

--- StateManagement/App.tsx
+++ StateManagement/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './slices';
-import AuthApp from './components/AuthApp';
+import TodoApp from './components/TodoApp';
 
 const store = createStore(rootReducer);
 
 function App() {
   return (
     <Provider store={store}>
-      <AuthApp />
+      <TodoApp />
     </Provider>
   );
 }
